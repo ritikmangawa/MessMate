856692698b6632a7212f5a117a2c68b6fe00bb7a
RITIK MANGAWA <ritikchoudhary0812@gmail.com>
2026-08-21 16:18:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true })); 
+app.use(cors({ origin: ['http://localhost:5173', 'https://mess-mate-alpha.vercel.app/'], credentials: true }));
 app.use(express.json()); 
 app.use(cookieParser()); 
 app.use('/api/auth', authRoutes);   
